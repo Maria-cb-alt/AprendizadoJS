d3b50833be06f8f9a742b0591434af4af3a49833
Maria-Cb-Alt <carolzinhabalassoni@gmail.com>
2022-12-08 18:58:51 -0300

    atualizações

---
+++ comparação/comparacao.js
@@ -0,0 +1,21 @@
+// == (comparação implicita)
+
+const numero = 5;
+const texto = "5"; 
+
+//console.log(numero == texto) --> Da true
+
+//console.log(numero === texto) --> Da false
+
+//typeof - Usado para descobrir o que está sendo usado na variável 
+
+console.log(typeof numero)
+console.log(typeof texto)
+
+//== só compara valor 
+// ===  compara valor e o tipo de dado 
+
+//conversões explicita 
+
+number()
+String()
---
+++ funcoes.js
@@ -0,0 +1,27 @@
+//let x = "";
+//console.log(x)
+//x = "oi";
+
+
+//Declaração de função
+//1) Declara a função 
+                     //string e 2 -> valor de soma()
+function imprimiTexto(texto) {
+    console.log(texto)
+}
+
+//2) Executa a função 1 ou mais vezes
+
+imprimiTexto(soma());
+imprimiTexto("outro texto");
+
+//Três formas de escrever funções
+
+//Usamos Return (e tem q ser obrigatoriamente a última linha da função) para retornar o valor já q n é passado um parametro para a nossa função 
+
+function soma(){
+    return 2 + 2;
+}
+
+//console.log(soma())
+
